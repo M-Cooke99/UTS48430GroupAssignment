8abda9844a48ee52a617b88cf41df03ced38c6dd
vtschouten97 <55377621+vtschouten97@users.noreply.github.com>
2019-09-19 13:50:27 +1000

    Copied from master to developer

--- timetable.c
+++ timetable.c
@@ -1,16 +1,89 @@
-#include <stdio.h>
+/******************************************************************************
+ * DESCRPITPION:
+******************************************************************************/
+
+/******************************************************************************
+ * HEADER FILES + PREPROCESSING DIRECTIVES
+******************************************************************************/
+#include <stdio.h> /*printf, scanf*/
+#include <string.h> /*strcmp*/
 #include "timetable.h" 
 
+/******************************************************************************
+ * STRUCTURES
+******************************************************************************/
+struct birthday
+{
+	int year, month, day;
+};
+typedef struct birthday birthday_t;
+
+struct address
+{
+	int houseNumber;
+	char [20] streetName;
+};
+typedef struct address address_t;
+
+struct personal
+{
+	birthday_t birthday;
+	address_t address;
+	long phoneNum;
+};
+typedef struct personal personal_t;
+
+struct student
+{
+	int number;
+	char [20] name;
+	char [20] password;
+	personal_t personalInfo;
+};
+typedef struct student student_t;
+
+struct time
+{
+	int day, hour, minute;
+};
+typedef struct time time_t;
+
+struct slot
+{
+	char [4] type;
+	char [20] lecturer;
+	int building, floor, room;
+	time_t start;
+	time_t end;
+};
+typedef struct slot slot_t;
+
+struct course
+{
+	int code;
+	char [20] lecturer;
+	slot_t slot0, slot1, slot2, slot3, slot4, slot5, slot6;
+};
+typedef struct course course_t;
 
 /******************************************************************************
  * FUNCTION PROTOTYPES
 ******************************************************************************/
+void getStuNum(long* stuNump);
+int checkStuNum(void);
 int studentMain(void);
+void printStuMenu(int* choicep);
+void printStuTimetable(int StuNum);
+void printStuDetails(int StuNum);
 int adminMain(void);
-void printStudentMenu(int* choicep);
 void printAdminMenu(int* choicep);
-
-
+void addStu(void);
+void removeStu(void);
+void editStu(void);
+void printStu(void);
+void printCourse(void);
+void printClass(void);
+void printEnrollment();
 /******************************************************************************
  * MAIN 
 ******************************************************************************/
@@ -32,30 +105,65 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+/******************************************************************************
+ * Asks for the user to enter a student number until a vaild one is entered
+ * Author: Michael
+ * IN: The storage location to save the student number
+ * OUT: None
+******************************************************************************/
+void getStuNum(long* stuNump){
+	do {printf ("Enter Student Number\n");
+		scanf ("%li",&stuNum);
+		
+		if (checkStuNum(stuNum) != 0)
+		{
+			printf("Student Number Invaild");	
+		}	
+		
+	}while (checkStuNum(stuNum) != 0);
+
+}
+
+/******************************************************************************
+ * Reads a student number and checks validity
+ * Author:
+ * IN: None
+ * OUT: 0 if successful
+******************************************************************************/
+int checkStuNum(void){ 
+
+}
+
 
 /******************************************************************************
  * Student main: menu options
+ * Author: Michael, Victor
+ * IN: None
+ * OUT: 0 if successful
 ******************************************************************************/
 int studentMain(void){
 	int choice;
+	long stuNum;
+	
+	getStuNum(&stuNum);
 
-	/* Verify Student ID */
-
-	do { printStudentMenu(&choice);
+	do { printStuMenu(&choice);
         switch (choice){
-            case 1: /* Timetable Printing Function */  break;
-            case 2: /* Personal Details Function */ break;
+            case 1: printStuTimetable(stuNum) break;
+            case 2: printStuDetails(stuNum) break;
             case 3: break;
             default: printf("Invalid choice\n");} 
     } while (choice != 3);
 	return 0;
 }
 
-
 /******************************************************************************
  * Prints student menu options
+ * Author: Victor
+ * IN: Pointer to 'choice' storage location
+ * OUT: None
 ******************************************************************************/
-void printStudentMenu(int* choicep){
+void printStuMenu(int* choicep){
 	printf("\n"
     "1. View Timetable\n"
     "2. View Personal Details\n"
@@ -64,72 +172,140 @@ void printStudentMenu(int* choicep){
     scanf("%d", choicep);
 }
 
+/******************************************************************************
+ * Prints the students timetable, chronologically per line
+ * Author:
+ * IN: Student number for the timetable to be printed
+ * OUT: 
+******************************************************************************/
+void printStuTimetable(int StuNum){
+	
+}
 
 /******************************************************************************
- * Read student number, password and check validity. maybe three seperate 
- * functions?
+ * Prints the students details
+ * Author:
+ * IN: The student number for the details to be printed
+ * OUT: None
 ******************************************************************************/
-
-
-/******************************************************************************
- * Timetable view function: prints the students timetable 
- * chronologically per line
-******************************************************************************/
-
-
-/******************************************************************************
- * Personal details function: prints the students details. Maybe give the
- * the student an option to edit his details? This is usually possible in
- * similar real world implementations
-******************************************************************************/
-
+void printStuDetails(int StuNum){
+	
+}
 
 /******************************************************************************
  * Admin main: menu options
+ * Author: Michael, Victor
+ * IN: None
+ * OUT: 0 if successful
 ******************************************************************************/
 int adminMain(void){
 	int choice;
 	
 	do { printAdminMenu(&choice);
         switch (choice){
-            case 1: /* Timetable Editing Function */  break;
-            case 2: /* Course Information Function */ break;
-            case 3: /* Show student list function */ break;
-            case 4: break;
+            case 1: void addStu(void) break;
+            case 2: void removeStu(void) break;
+            case 3: void editStu(void) break;
+            case 4: void printStu(void) break;
+			case 5: void printCourse(void) break;
+			case 6: void printClass(void) break;
+			case 7: void printEnrollment() break;
+			case 8: break;
             default: printf("Invalid choice\n");} 
-    } while (choice != 4);
+    } while (choice != 8);
 	return 0;
 }
 
 
 /******************************************************************************
  * Prints admin menu options 
+ * Author: Michael, Victor
+ * IN: Pointer to 'choice' storage location
+ * OUT: None
 ******************************************************************************/
 void printAdminMenu(int* choicep){
 	printf("\n"
-    "1. Edit Timetable\n"
-    "2. Show Course Information\n"
-    "3. Show all enrolled students\n"
-    "4. Exit Program\n"
-    "Enter Choice (number between 1-4)>\n");
+ 	"1. Add Student\n"
+	"2. Remove Student\n"
+	"3. Edit Student\n"
+    "4. View Student Timetable and Details\n"
+    "5. Show Course Information\n"
+	"6. Show Class information\n"
+    "7. Show all enrolled students\n"
+    "8. Exit Program\n"
+    "Enter Choice (number between 1-8)>\n");
     scanf("%d", choicep);
 }
 
+/******************************************************************************
+ * Add student function
+ * Author:
+ * IN: None
+ * OUT: None
+******************************************************************************/
+void addStu(void){
+	
+}
 
 /******************************************************************************
- * Timetable edit function: This will be a big function, try to keep it modular
- * Swap, add or remove students from course timetable. ... More?
+ * Remove student function
+ * Author:
+ * IN: None
+ * OUT: None
 ******************************************************************************/
-
+void removeStu(void){
+	
+}
 
 /******************************************************************************
- * View Course Information function: prints all courses' lectures and
- * tutorials with amount of students. Possibly add option to select a course to
- * view its members?
+ * Edit student details function
+ * Author:
+ * IN: None
+ * OUT: None
 ******************************************************************************/
-
+void editStu(void){
+	
+}
 
 /******************************************************************************
- * View all enrolled students function: prints list of all students enrolled.
- * Possibly add option to select a student to view all its enrolled courses?
+ * Prints a students details followed by their timetable
+ * Author: Michael
+ * IN: None
+ * OUT: None
 ******************************************************************************/
+void printStu(void){
+	long stuNum;
+	getStuNum(&stuNum);
+	printStuDetails(stuNum);
+	printf("\n");
+	printStuTimetable(stuNum);
+}
+
+/******************************************************************************
+ * Prints all courses' lectures and tutorials with amount of students
+ * Author:
+ * IN: None
+ * OUT: None
+******************************************************************************/
+void printCourse(void){
+	
+}
+
+/******************************************************************************
+ * Prints the names and number of students enrolled in a given Lab/Lec/Tut 
+ * Author:
+ * IN: None
+ * OUT: None
+******************************************************************************/
+void printClass(void){
+	
+}
+/******************************************************************************
+ * Prints the number of students enrolled and a list of their student numbers
+ * Author:
+ * IN: None
+ * OUT: None
+******************************************************************************/
+void printEnrollment(void){
+	
+}
